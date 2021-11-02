8c01d0c3d6b1118406b03191805a2567bffcd9e2
Anderson Walsh <andy@Andersons-MacBook-Air.local>
2021-11-02 14:53:16 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+'''
+Andy Walsh
+Tic Tac Toe
+Sep. 21
+'''
+
+print("Welcome to Tic-Tac-Toe")
+print("  |   |   ")
+print("_   _   _ ")
+print("  |   |   ")
+print("_   _   _ ")
+print("  |   |   ")
+
+Board = ["  |", "  ", "|  ", "_  ", " _ ", "  _"]
+len =
+for i in range(2):
+    print(board)
+    
+    
